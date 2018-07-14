d38ffb19e215492ffa1fa6c0cdb74fe913a7f8d2
Kazuhito Tsujita <kazuhito.tsujita@gmail.com>
2018-07-14 18:07:33 +0900

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var postcssGapProperties = require('postcss-gap-properties');
+
+gulp.task('sass', function () {
+  return gulp.src('./src/scss/*.scss')
+    .pipe(sass({
+      outputStyle: 'compressed'
+    }).on('error', sass.logError))
+    .pipe(postcss([
+      postcssGapProperties(),
+      autoprefixer({
+        browsers: ['last 2 major versions', 'ie >= 11'],
+        grid: true,
+        cascade: false
+      })
+    ]))
+    .pipe(gulp.dest('./docs/css/'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./src/scss/*.scss', ['sass']);
+});
+
+// Static server
+gulp.task('browser-sync', function() {
+  browserSync.init({
+    server: {
+      baseDir: "./docs"
+    }
+  });
+});
+
+gulp.task('bs-reload', function () {
+  browserSync.reload();
+});
+
+gulp.task('default', ['browser-sync','sass:watch'], function () {
+  gulp.watch("./docs/*.html", ['bs-reload']);
+  gulp.watch("./docs/css/*.css", ['bs-reload']);
+});
